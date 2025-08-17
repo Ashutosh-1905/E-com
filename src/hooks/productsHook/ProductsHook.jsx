2134c3ab1a4d9f6ec398e0152761371b33e2c8eb
Ashutosh Dhakad <ashutoshatoliya@gmail.com>
2025-08-17 07:34:46 +0530

    dummy rafce for import and export files easy

---
+++ src/hooks/productsHook/ProductsHook.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductsHook = () => {
+  return (
+    <div>ProductsHook</div>
+  )
+}
+
+export default ProductsHook
---
+++ src/screens/adminScreens/AllProductsScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllProductsScreen = () => {
+  return (
+    <div>AllProductsScreen</div>
+  )
+}
+
+export default AllProductsScreen

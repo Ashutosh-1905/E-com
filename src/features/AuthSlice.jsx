20fd0fe38b508281705c79251e3a3fc080c91b5c
Ashutosh Dhakad <ashutoshatoliya@gmail.com>
2025-08-17 17:28:16 +0530

    stores user authentication data.

--- src/features/AuthSlice.jsx
+++ src/features/AuthSlice.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { createSlice } from "@reduxjs/toolkit";
 
-const AuthSlice = () => {
-  return (
-    <div>AuthSlice</div>
-  )
-}
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  isLoggedIn: "",
+  isAdmin:false,
+};
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
 
-export default AuthSlice
+  reducers: {
+    addUser: (state, action) => {
+      const { username, email, password } = action.payload;
+      state.username = username,
+      state.email = email,
+      state.password = password,
+      state.isLoggedIn = true,
+      state.isAdmin = false
+    },
+
+    deleteUser: (state) => {
+      (state.username = ""),
+        (state.email = ""),
+        (state.password = ""),
+        (state.isLoggedIn = false),
+        (state.isAdmin = false);
+    },
+
+  },
+});
+
+
+export const { addUser, deleteUser } = authSlice.actions;
+
+export default authSlice.reducer;
+
